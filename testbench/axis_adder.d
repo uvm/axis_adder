ba8dab362d7d3e100c764f373b787b26de5c01c8
Puneet Goel <puneet@coverify.com>
2025-09-18 00:08:11 +0530

    Added a random sequence and the corresponding test

--- testbench/axis_adder.d
+++ testbench/axis_adder.d
@@ -127,7 +127,17 @@ class uvm_adder_tb: uvm_context
 }
 
 void main(string[] args) {
+  import std.stdio;
+  uint random_seed;
+
+  CommandLine cmdl = new CommandLine(args);
+
+  if (cmdl.plusArgs("random_seed=" ~ "%d", random_seed))
+    writeln("Using random_seed: ", random_seed);
+  else random_seed = 1;
+
   auto tb = new uvm_adder_tb;
   tb.elaborate("tb", args);
+  tb.set_seed(random_seed);
   tb.start();
 }
